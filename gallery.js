23e6f0048e3d0b863bd9f84c155a76c632725e83
Sarah <sarah.sidlow@georgetown.edu>
2019-02-14 13:52:58 -0500

    Add on click functions

--- gallery.js
+++ gallery.js
@@ -7,9 +7,41 @@ $(document).ready(function() {
 
 
 	$('.slide').not(':first').hide();//hide all images except first one
+	$('.previous').on('click', function(){
+        // Change to the previous image
+        
+        $('.slide' + currentImage).stop().fadeOut(1);
+        decreaseImage();
+        $('.slide' + currentImage).stop().fadeIn(1);
+    }); 
+$('.next').on('click', function(){
+    // Change to the next image
+    $('.slide' + currentImage).stop().fadeOut(1);
+    increaseImage();
+    $('.slide' + currentImage).stop().fadeIn(1);
+
+  }); 
 
 
-
+	function increaseImage() {
+    /* Increase currentImage by 1.
+    * Resets to 1 if larger than totalImages
+    */
+  ++currentImage;
+    if(currentImage > totalImages) {
+      currentImage = 1;
+    }
+  }
+  function decreaseImage() {
+    /* Decrease currentImage by 1.
+    * Resets to totalImages if smaller than 1
+    */
+    --currentImage;
+    if(currentImage < 1) {
+      currentImage = totalImages;
+    }
+  }
+});
 
 
 /** $(document).ready(function() {
