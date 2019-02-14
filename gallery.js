8ef44d696a2707b01eddb000c03b60f8de0ff02d
Sarah <sarah.sidlow@georgetown.edu>
2019-02-14 13:51:38 -0500

    Define Variables and Hide all but first slide

--- gallery.js
+++ gallery.js
@@ -1,59 +1,12 @@
 //document ready function
 $(document).ready(function() {
-		setImageNumber();
+    setImageNumber();
 	var slide = $('.slide')//define variables
 	var currentImage = 1;
 	var totalImages = slide.length;
 
 
 	$('.slide').not(':first').hide();//hide all images except first one
-	$('.previous').on('click', function(){
-        // Change to the previous image
-        
-        $('.slide' + currentImage).stop().fadeOut(1);
-        decreaseImage();
-        $('.slide' + currentImage).stop().fadeIn(1);
-    }); 
-$('.next').on('click', function(){
-    // Change to the next image
-    $('.slide' + currentImage).stop().fadeOut(1);
-    increaseImage();
-    $('.slide' + currentImage).stop().fadeIn(1);
-
-  }); 
-
-  
-
-  function increaseImage() {
-    /* Increase currentImage by 1.
-    * Resets to 1 if larger than totalImages
-    */
-  ++currentImage;
-    if(currentImage > totalImages) {
-      currentImage = 1;
-    }
-  }
-  function decreaseImage() {
-    /* Decrease currentImage by 1.
-    * Resets to totalImages if smaller than 1
-    */
-    --currentImage;
-    if(currentImage < 1) {
-      currentImage = totalImages;
-    }
-  }
-    function setImageNumber() {
-        var count = 1;
-      $('.slide').each(function(){
-          var cl = 'slide'+count;
-          $(this).addClass(cl);
-          count++;
-        });
-    }
-});
-
-
-
 
 
 
